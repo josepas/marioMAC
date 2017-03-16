237d8f612deb7a68fca7814f91b2f546acbb5e82
Jose Pascarella Quijada <josepas979@gmail.com>
2017-03-15 22:14:40 -0400

    Lista la logica detras de la tabla de scores

--- scores/views.py
+++ scores/views.py
@@ -1,8 +1,32 @@
 from django.http import HttpResponse
+from django.shortcuts import get_list_or_404
+from preguntas.models import Equipo, Pregunta, Respuesta
 from django.shortcuts import render
 
 # Create your views here.
 
+def get_answers(equipo):
+	
+	result = []
+	
+	preguntas = Pregunta.objects.all()
+	for pregunta in preguntas:
+		answer = Respuesta.objects.get(equipo=equipo, pregunta=pregunta)
+		result.append(answer)
+
+	return result
+
+
 def index(request):
 
-	return render(request, 'score_index.html')
+	es = Equipo.objects.all()
+	equipos = {}
+	for equipo in es:
+		equipos[equipo.name] = get_answers(equipo)
+
+	context = {
+		'equipos' : equipos
+	}
+
+	return render(request, context)
+
